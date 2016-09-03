12ddd9b18ba616225e86d95fbbceec73cbdc7bc7
Kevin Niemann <kevin.niemann@gmail.com>
2016-09-03 14:33:08 -0700

    Initial commit.

---
+++ src/main/java/org/kan/kafka/KafkaInterpreter.java
@@ -0,0 +1,60 @@
+package org.kan.kafka;
+
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.zeppelin.interpreter.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Arrays;
+import java.util.Properties;
+
+/**
+ * Created by kevin on 9/3/16.
+ */
+public class KafkaInterpreter extends Interpreter{
+    private static final Logger LOGGER = LoggerFactory.getLogger(KafkaInterpreter.class);
+    private KafkaConsumer consumer;
+
+    public KafkaInterpreter(Properties props) {
+        super(props);
+    }
+
+    @Override
+    public void open() {
+        logger.info("Opening Kafka Consumer");
+        Properties consumerProps = new Properties();
+        //consumerProps.setProperty();
+        consumer = new KafkaConsumer(consumerProps);
+    }
+
+    @Override
+    public void close() {
+        LOGGER.info("Closing Kafka Consumer");
+        consumer.close();
+    }
+
+    @Override
+    public InterpreterResult interpret(String topic, InterpreterContext interpreterContext) {
+        logger.info("Subscribe to topic: " + topic);
+        consumer.subscribe(Arrays.asList("topic"));
+        InterpreterResult result = new InterpreterResult(InterpreterResult.Code.SUCCESS);
+        return null;
+    }
+
+    @Override
+    public void cancel(InterpreterContext interpreterContext) {
+        LOGGER.info("Canceling Kafka Consumer");
+        consumer.close();
+
+    }
+
+    @Override
+    public FormType getFormType() {
+        return FormType.NONE;
+    }
+
+    @Override
+    public int getProgress(InterpreterContext interpreterContext) {
+        return 0;
+    }
+}
